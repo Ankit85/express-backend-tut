96ade0780471eaf05088d41c209f77d0630ab913
Ankit <ankit60.av@gmail.com>
2024-06-12 12:59:11 +0530

    mongodb connection add

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+  try {
+    if (process.env.MONGODB_URI === undefined) {
+      throw new Error("MONGODB_URI variable not declared or is undefined");
+    }
+
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log("MONGODB connected successfully!!!");
+    console.log(
+      `MONGODB connection host uri: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection failed: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
